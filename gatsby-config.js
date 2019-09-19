4e719d01d7d7c4703d7ab1aa34ca5dfa761101ce
Pravinthan Prabagaran <pravinthan.prabagaran@gmail.com>
2019-09-19 02:01:25 -0400

    Added analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,14 +25,6 @@ module.exports = {
       resolve: "gatsby-plugin-google-analytics",
       options: {
         trackingId: "UA-148331663-1",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
       },
     },
     {
